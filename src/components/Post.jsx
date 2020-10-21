db90352d4dfbfe95f52addad831de1d2d5ee919d
Nicola-9 <Nicola-9@users.noreply.github.com>
2020-10-21 14:12:57 +0200

    Add basic component and other files

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Post.css';
+
+export default class Post extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
